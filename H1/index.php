e916da0c872b03199774a95f6edb8291c0bde92f
Chester <2133807@talnet.nl>
2021-03-09 13:27:01 +0100

    H1

---
+++ H1/index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+        $a = "Ik heb niet gefaald";
+        $b = "Ik heb alleen";
+        $c = "10000 manieren gevonden";
+        $d = "Die niet werken";
+        $e = "-Thomas Edison";
+        $volledigeZin = $a . $b . $c . $d . "<br>". $e;
+
+        echo $volledigeZin;
+
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+        $f = "Mensen die geen";
+        $g = "fouten maken,";
+        $h = "werken niet";
+        $i = "-Albert Plesman (KLM)";
+        $volledigeZin3 = $f . $g . $h . "<br>". $i ;
+
+        echo $volledigeZin3;
+        echo "<br>";
+
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+        $j = "Try and fail,";
+        $k = " but never";
+        $l = " fail to try";
+        $m = " -Tyra Banks";
+        $volledigeZin5 =  $j . $k . $l . "<br>". $m;
+
+        echo $volledigeZin5;
+        echo "<br>";
+
+        $n = "De cursus “omgaan met teleurstellingen”";
+        $o = "kan vanavond ";
+        $p = "helaas niet doorgaan";
+        $q = "- Herman Finkers";
+        $volledigeZin7 =  $n . $o . $p . "<br>" . $q;
