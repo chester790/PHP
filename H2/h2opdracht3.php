7928e0b6b8e835b09a45d5499981b0527542edd7
Chester <2133807@talnet.nl>
2021-03-10 13:07:15 +0100

    H2 assigment 3 done

---
+++ H2/h2opdracht3.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        PHP opdracht 2
+    </title>
+    <style>
+    </style>
+</head>
+<body>
+<?php
+    for ($x = 35; $x >=25; $x--) {
+        echo "hoppelepee";
+    }
+?>
+</body>
+</html>
