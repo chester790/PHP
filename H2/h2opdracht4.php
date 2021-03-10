c81a6a77b545b5fc672b8ecac83267dd794ef155
Chester <2133807@talnet.nl>
2021-03-10 13:24:24 +0100

    H2 assigment 4 done

---
+++ H2/h2opdracht4.php
@@ -0,0 +1,35 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        PHP opdracht 2
+    </title>
+    <style>
+        body {
+            text-align: center;
+        }
+
+        .rood {
+            border: red solid 5px;
+        }
+        .groen {
+            border: green solid 5px;
+        }
+    </style>
+</head>
+<body>
+<?php
+for($i = 1; $i <=9; $i++) {
+    if($i % 2 == 0) {
+        $class = "class='rood'";
+    } else {
+        $class = "class='groen'";
+    }
+    echo "<img ".$class. "src='img/apen/aap".$i.".jpg'>";
+}
+?>
+</body>
+</html>
