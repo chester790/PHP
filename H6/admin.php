81805383d6b050ce8e15fe286056456dfc7405db
Chester <2133807@talnet.nl>
2021-04-28 10:35:50 +0200

    Namen verandert

--- H6/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== "admin") {
-    echo "<script>window.location.href='index.php';</script>";
-    exit;
-} else {
-    echo "geheime tekst";
-}
--- H6/index.php
+++ H6/index.php
@@ -1,35 +1,11 @@
+
 <?php
-session_start();
 $db = new mysqli("localhost","root","","school","3306");
-echo "<form method='POST'>
-    <input type='text' name='email' placeholder='email'>
-    <input type='password' name='password' placeholder='password'>
-    <input type='submit' name='submit' value='login'>
-</form>";
+$query = "SELECT naam FROM cursist";
+$output = mysqli_query($db, $query);
+echo "<table border='1'><tr>";
+$data = mysqli_fetch_array($output);
+    echo "<td>" . $data['naam'] . "</td>";
+echo "</tr></table>";
 
-
-if (isset($_POST['submit'])) {
-    $query = "SELECT * FROM account WHERE email='" . $_POST['email'] ."' AND password='" . $_POST['password']. "'";
-    $output = mysqli_query($db, $query);
-    $data = mysqli_fetch_array($output);
-
-    if ($data['email'] === $_POST['email'] && $data['password'] === $_POST['password']) {
-        $_SESSION['role'] = $data['role'];
-        $_SESSION['logged'] = True;
-        $_SESSION['email'] = $data['email'];
-    } else {
-        echo "email/wachtwoord klopt niet";
-    }
-}
-
-if(isset($_SESSION['role'])) {
-    if ($_SESSION['role'] === "admin") {
-        echo "hallo admin";
-    } else {
-        echo "hallo gebruiker";
-    }
-} else {
-    echo"Hallo gast";
-}
-
-echo "<a href='admin.php'>admin page</a>";
+include("login.php");
