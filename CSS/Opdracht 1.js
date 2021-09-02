88173327ce23dfc88af3af356439feb0fec7ba7e
Chester <2133807@talnet.nl>
2021-09-02 12:15:47 +0200

    nieuwe opdracht school 2e jaars

---
+++ CSS/Opdracht 1.js
@@ -0,0 +1,14 @@
+function getAge(birthDateString) {
+    var today = new Date();
+    var birthDate = new Date(birthDateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+if(getAge("01/01/2003") >= 18) {
+    alert("True!");
+}
